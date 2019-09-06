ef30e5c758a63e56aa66d85f046fbe04f5c2c26a
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-09-07 01:40:01 +0530

    Mesaage heading display, Timer show and alignment settings

--- show.js
+++ show.js
@@ -14,6 +14,11 @@ function message(){
     document.getElementById("time").innerHTML=currentdate.getHours()+":"+currentdate.getMinutes();
     document.getElementById('messagetitle').style.left = "0%";
     document.getElementById('welcomeTime').style.display = "none";
+    document.getElementById("messagebox1").style.display="block";
+    document.getElementById("messagebox2").style.display="block";
+    document.getElementById("messagebox3").style.display="block";
+
+
 }
 function music(){
     document.getElementById("music").style.backgroundColor="aqua";
@@ -23,8 +28,14 @@ function music(){
     document.getElementById("musictitle").style.display="block";
     document.getElementById("timertitle").style.display="none";
     document.getElementById("time").style.left="300%";
+    document.getElementById("time").firstChild.innerHTML=currentdate.getHours()+":"+currentdate.getMinutes();
     document.getElementById("musictitle").style.right = "10%";
     document.getElementById("welcomeTime").style.display="none";
+    document.getElementById("messagebox1").style.display="none";
+    document.getElementById("messagebox2").style.display="none";
+    document.getElementById("messagebox3").style.display="none";
+
+
 }
 function timer(){
     document.getElementById("music").style.backgroundColor="white";
@@ -36,6 +47,28 @@ function timer(){
     document.getElementById("time").style.left="300%";
     document.getElementById("musictitle").style.right = "10%";
     document.getElementById("welcomeTime").style.display="none";
+    document.getElementById("messagebox1").style.display="none";
+    document.getElementById("messagebox2").style.display="none";
+    document.getElementById("messagebox3").style.display="none";
+
 
     
 }
+function start(){
+    var deadline = new Date("Sep 7, 2019 23:59:59").getTime(); 
+    var x = setInterval(function() { 
+    var now = new Date().getTime(); 
+    var t = deadline-now; 
+    //var days = Math.floor(t / (1000 * 60 * 60 * 24)); 
+    var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60)); 
+    var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)); 
+    var seconds = Math.floor((t % (1000 * 60)) / 1000); 
+    document.getElementById("countdown").innerHTML =  
+     hours + ":" + minutes + ":" + seconds; 
+    if (t < 0) 
+        { 
+        clearInterval(x); 
+        document.getElementById("demo").innerHTML = "EXPIRED"; 
+        } 
+    }, 1000); 
+    }
