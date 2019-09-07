4fa01788217fa28961941909c5f9850957a8bc7a
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-09-07 14:18:14 +0530

    some changes about the alignment

--- show.js
+++ show.js
@@ -3,6 +3,13 @@ $(document).ready(function(){
     document.getElementById("welcomeTime").innerHTML=currentdate.getHours()+":"+currentdate.getMinutes();
 
 })
+
+let arr=[{id:"1",from:"sasank", msg:"Hi, good morning"},
+        {id:"2",from:"saurav", msg:"Hi ,good evening"},
+        {id:"3",from:"puneet", msg:"Hi ,good afternoon"}
+        ];
+        
+        
 function message(){
     document.getElementById("message").style.backgroundColor="aqua";
     document.getElementById("music").style.backgroundColor="white";
@@ -20,6 +27,7 @@ function message(){
 
 
 }
+
 function music(){
     document.getElementById("music").style.backgroundColor="aqua";
     document.getElementById("message").style.backgroundColor="white";
@@ -54,6 +62,7 @@ function timer(){
 
     
 }
+  
 function start(){
     var deadline = new Date("Sep 7, 2019 23:59:59").getTime(); 
     var x = setInterval(function() { 
@@ -71,4 +80,95 @@ function start(){
         document.getElementById("demo").innerHTML = "EXPIRED"; 
         } 
     }, 1000); 
-    }
+
+
+//     var timer = document.getElementById('countdown');
+// var toggleBtn = document.getElementById('start');
+// var resetBtn = document.getElementById('reset');
+
+// var watch = new Stopwatch(timer);
+
+// function start() {
+//   toggleBtn.textContent = 'Stop';
+//   watch.start();
+// }
+
+// function stop() {
+//   toggleBtn.textContent = 'Start';
+//   watch.stop();
+// }
+
+// toggleBtn.addEventListener('click', function() {
+//   watch.isOn ? stop() : start();
+// });
+
+// resetBtn.addEventListener('click', function() {
+//   watch.reset();
+// })
+
+// function Stopwatch(elem) {
+//     var time = 0;
+//     var offset;
+//     var interval;
+  
+//     function update() {
+//       if (this.isOn) {
+//         time += delta();
+//       }
+      
+//       elem.textContent = timeFormatter(time);
+//     }
+  
+//     function delta() {
+//       var now = Date.now();
+//       var timePassed = now - offset;
+  
+//       offset = now;
+  
+//       return timePassed;
+//     }
+  
+//     function timeFormatter(time) {
+//       time = new Date(time);
+  
+//       var minutes = time.getMinutes().toString();
+//       var seconds = time.getSeconds().toString();
+//       var milliseconds = time.getMilliseconds().toString();
+  
+//       if (minutes.length < 2) {
+//         minutes = '0' + minutes;
+//       }
+  
+//       if (seconds.length < 2) {
+//         seconds = '0' + seconds;
+//       }
+  
+//       while (milliseconds.length < 3) {
+//         milliseconds = '0' + milliseconds;
+//       }
+  
+//       return minutes + ' : ' + seconds + ' . ' + milliseconds;
+//     }
+  
+//     this.start = function() {
+//       interval = setInterval(update.bind(this), 10);
+//       offset = Date.now();
+//       this.isOn = true;
+//     };
+  
+//     this.stop = function() {
+//       clearInterval(interval);
+//       interval = null;
+//       this.isOn = false;
+//     };
+  
+//     this.reset = function() {
+//       time = 0;
+//       update();
+//     };
+  
+//     this.isOn = false;
+//   }
+}
+
+    
